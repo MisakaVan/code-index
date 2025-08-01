18fed9b0f8cc9dca6f15b6a97863bfef3690fbbb
MisakaVan <2102315149@qq.com>
2025-08-02 01:12:18 +0800

    doc: update napoleon settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,12 +35,12 @@ extensions = [
 # Napoleon settings for Google-style docstrings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
 napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
